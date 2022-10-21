663660bcd69e52075239a2c76aa67bad68a0565f
Ariel Emilio Martino Comba <arielmartino23@hotmail.com>
2022-10-21 17:19:46 -0300

    se agrego evento click, falta que no agreguen mas del stock

--- src/app/server-list/server-list.component.ts
+++ src/app/server-list/server-list.component.ts
@@ -17,7 +17,8 @@ export class ServerListComponent implements OnInit {
       Precio: 1000,
       image:"assets/kelthuzad.jpg",
       onsale: false,
-
+      available: 5,
+      quantity: 0,
     },
     {
       nombre:"Chilling reign",
@@ -26,6 +27,8 @@ export class ServerListComponent implements OnInit {
       Precio: 500,
       image:"assets/kelthuzad.jpg",
       onsale: true,
+      available: 2,
+      quantity: 0,
     },
     {
       nombre:"Agaton",
@@ -34,6 +37,8 @@ export class ServerListComponent implements OnInit {
       Precio: 0,
       image:"assets/kelthuzad.jpg",
       onsale: false,
+      available: 0,
+      quantity: 0,
     }
   ];
   constructor() { }
@@ -41,4 +46,21 @@ export class ServerListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  upQuantity(server : server): void {
+    if(server.quantity < server.available)
+      server.quantity++;
+  }
+
+  downQuantity(server : server): void {
+    if(server.quantity > 0)
+      server.quantity--;
+  }
+
+  onChangeQuantity(event, server : server): void{
+    //console.log(event);
+    if(event.key > server.available){
+      alert('No se puede pedir mas de los disponibles');
+      event.preventDefault();
+    }
+  }
 }
--- src/app/server-list/server.ts
+++ src/app/server-list/server.ts
@@ -5,4 +5,6 @@ export interface server{
     Precio : number;
     image : string;
     onsale : boolean;
+    available: number;
+    quantity: number;
 }
